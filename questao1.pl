4989005d8cf9cb46468b1a442ae9bed6b2c745e3
Karine Pires <Karine Pires>
2017-07-11 23:00:03 -0300

    Implementacao parcial da questao

--- questao1.pl
+++ questao1.pl
@@ -33,3 +33,17 @@ filha(X,Y) :- pai(Y, X) ; mae(Y, X).
 % REGRA IRMÃO E IRMÃ:
 irmao(X, Y) :- ((pai(Z, X) ; mae(Z, X)) , (pai(Z, Y) ; mae(Z, Y)) , homem(X)).
 irma(X, Y) :- ((pai(Z, X) ; mae(Z, X)) , (pai(Z, Y) ; mae(Z, Y)) , mulher(X)).
+%REGRA: TIO E TIA:
+tio(X, Y) :- ((pai(W, Y) ; mae(W, Y)) , (irmao(X, W) ; irma(W, X)) , homem(X)).
+tia(X, Y) :- ((pai(W, Y) ; mae(W, Y)) , (irmao(W, X) ; irma(X, W)) , mulher(X)).
+%REGRA: SOBRINHO E SOBRINHA
+sobrinho(X, Y) :- pai(W, X) , (irmao(W, Y) ; irma(W, Y)), homem(X).
+sobrinha(X, Y) :- pai(W, X) , (irmao(W, Y) ; irma(W, Y)), mulher(X).
+%REGRA: PRIMO E PRIMA
+primo(X, Y) :- pai(W, X) , pai(Z, Y) , (irmao(Z, W) ; irma(W, Z)) , homem(X).
+prima(X, Y) :- pai(W, X) , pai(Z, Y) , (irmao(Z, W) ; irma(W, Z)) , mulher(X).
+%REGRA: AVÔ E AVÓ
+avô(X, Y) :- (pai(W, Y) ; mae(W, Y)) , (filho(W, X) ; filha(W, X)), homem(X).
+avó(X, Y) :- (pai(W, Y) ; mae(W, Y)) , (filho(W, X) ; filha(W, X)), mulher(X).
+%REGRA: CUNHADO E CUNHADA
+cunhado(X, Y) :-
